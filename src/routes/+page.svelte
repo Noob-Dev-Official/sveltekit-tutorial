0ea71df6b639e097c1b346022e724b1a3f838d82
noob_dev54 <muntasirenamalhuda@gmail.com>
2023-05-13 11:12:20 +0100

    initialised svelte project; done on:click event for bttn and bind:value event for input

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	let title = 'hello';
+
+	const updateTitle = () => {
+		title = 'different';
+	};
+</script>
+
+<div class="index">
+	<h2>Some Title</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ad alias
+		quos totam magnam eos sequi numquam sit doloribus dolore {title}.
+	</p>
+
+	<button on:click={updateTitle}>Change Title</button>
+
+	<input type="text" bind:value={title} />
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		color: var(--red-color);
+	}
+</style>
