3d3c7823921726a7d2864882f9cc5c44bf390743
noob_dev54 <muntasirenamalhuda@gmail.com>
2023-05-15 16:02:25 +0100

    post request

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -5,3 +5,18 @@ import { comments } from '$lib/comments';
 export const GET = () => {
 	return json(comments);
 };
+
+export const POST = async (req) => {
+	const { request } = req;
+
+	const { text } = await request.json();
+
+	const newComment = {
+		id: comments.length + 1,
+		text,
+	};
+
+	comments.push(newComment);
+
+	return json(newComment, { status: 200 });
+};
